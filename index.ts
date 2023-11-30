79a029cecca3ff20e79e276f11d66a7d306bc654
HundredPercentCoverage <bunglenutter@gmail.com>
2023-11-30 20:03:01 +0000

    Passport stuff

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 import "dotenv/config";
 import express from "express";
+import session from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
 import ejs from "ejs";
 import path from "path";
 import { PrismaClient } from "@prisma/client";
+import authRouter from "./routes/auth";
+import passport from "passport";
 
 const prisma = new PrismaClient();
 const app = express();
@@ -10,11 +14,30 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    },
+    secret: process.env.COOKIE_SECRET as string,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+app.use(passport.authenticate("session"));
+
+app.use("/auth", authRouter);
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-app.get("/", async (_, res) => {
+app.get("/", async (req, res) => {
+  console.log("/ user", req.user);
   const todos = await prisma.todo.findMany();
   res.render("index", { title: "Home", message: "ToDo", todos });
 });
---
+++ routes/auth.ts
@@ -0,0 +1,64 @@
+import express from "express";
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import { pbkdf2, timingSafeEqual } from "node:crypto";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+passport.use(
+  new LocalStrategy.Strategy(async function verify(username, password, cb) {
+    const user = await prisma.user.findUnique({ where: { username } });
+
+    if (!user)
+      return cb(null, false, { message: "Incorrect username or password." });
+
+    pbkdf2(password, user.salt, 310000, 32, "sha256", (err, pwHash) => {
+      if (err) return cb(err);
+
+      if (!timingSafeEqual(user.hashed_password, pwHash))
+        return cb(null, false, { message: "Incorrect username or password." });
+
+      return cb(null, user);
+    });
+  })
+);
+
+// Types here are sketchy
+passport.serializeUser(function (user: any, done) {
+  console.log("serializeUser");
+  // process.nextTick(function () {
+  done(null, { id: user.id, username: user.username });
+  // });
+});
+
+passport.deserializeUser(function (user: any, done) {
+  console.log("deserialise", user);
+  process.nextTick(async function () {
+    return done(null, user);
+  });
+});
+
+const router = express.Router();
+
+router.get("/login", (req, res) => {
+  res.render("login", { title: "Login" });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth/login",
+  })
+);
+
+router.post("/logout", (req, res, next) => {
+  req.logOut(function (err) {
+    if (err) return next(err);
+    res.setHeader("HX-Redirect", "/auth/login");
+    res.send("");
+  });
+});
+
+export default router;
