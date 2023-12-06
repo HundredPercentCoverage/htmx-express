b0bd61549851ff9fb8836fe0e56ed8478892e6b0
HundredPercentCoverage <bunglenutter@gmail.com>
2023-12-06 21:39:32 +0000

    Flesh out

--- views/pages/HomePage.tsx
+++ views/pages/HomePage.tsx
@@ -1,10 +1,21 @@
+import { Todo } from "@prisma/client";
 import RootLayout from "./layout";
+import TodoList from "../components/TodoList";
 
-const HomePage: React.FC = () => {
+interface Props {
+  todos: Todo[];
+}
+
+const HomePage = ({ todos }: Props) => {
   return (
     <RootLayout title="Home">
-      <main>
-        <div>stuff</div>
+      <main className="container">
+        <h1>My Todo List</h1>
+        <p>Todos:</p>
+        <TodoList todos={todos} />
+        <button hx-post="/todos" hx-target="#todo-list" hx-swap="beforeend">
+          Add Todo
+        </button>
       </main>
     </RootLayout>
   );
