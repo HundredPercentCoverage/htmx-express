f1da09c3fd933c5fff8f02968acd63b257ed460e
HundredPercentCoverage <bunglenutter@gmail.com>
2023-12-11 21:55:54 +0000

    Fix usual error

--- views/components/TodoList.tsx
+++ views/components/TodoList.tsx
@@ -9,7 +9,7 @@ const TodoList = ({ todos }: Props) => {
   return (
     <ul className="todo-list" id="todo-list">
       {todos.map((todo) => (
-        <TodoItem todo={todo} />
+        <TodoItem todo={todo} key={todo.id} />
       ))}
     </ul>
   );
